65486f20c2fe8376a17d8ebe6358bfa8874c86eb
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-11-05 19:43:55 +0900

    version 0.9.2

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ URL="https://github.com/KoichiYasuoka/esupar"
 
 setuptools.setup(
   name="esupar",
-  version="0.9.1",
-  description="Tokenizer POS-tagger and Dependency-parser with BERT/RoBERTa models for Japanese, Chinese and Thai Language",
+  version="0.9.2",
+  description="Tokenizer POS-tagger and Dependency-parser with BERT/RoBERTa models for Japanese Chinese and Thai Language",
   long_description=long_description,
   long_description_content_type="text/markdown",
   url=URL,
