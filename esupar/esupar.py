ae2bb488439b822dba21954b772da1dd960ec188
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-12-20 16:56:44 +0900

    ja_luw_small

--- esupar/esupar.py
+++ esupar/esupar.py
@@ -5,7 +5,7 @@ MODELS={
   "ja":"KoichiYasuoka/bert-base-japanese-upos",
   "ja_base":"KoichiYasuoka/bert-base-japanese-upos",
   "ja_large":"KoichiYasuoka/bert-large-japanese-upos",
-  "ja_luw":"KoichiYasuoka/roberta-small-japanese-luw-upos",
+  "ja_luw":"KoichiYasuoka/bert-base-japanese-luw-upos",
   "ja_luw_small":"KoichiYasuoka/roberta-small-japanese-luw-upos",
   "ja_luw_base":"KoichiYasuoka/bert-base-japanese-luw-upos",
   "ja_luw_large":"KoichiYasuoka/bert-large-japanese-luw-upos",
