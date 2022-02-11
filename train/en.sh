dd1cb46a9ae6e028834f42bd971953ab6703816a
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-02-11 15:16:38 +0900

    pytokenizations

--- train/en.sh
+++ train/en.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-pip3 install -U esupar
+pip3 install -U esupar pytokenizations
 python3 -m esupar.train roberta-base KoichiYasuoka/roberta-base-english-upos https://github.com/UniversalDependencies/UD_English-EWT
 python3 -m esupar.train xlm-roberta-base KoichiYasuoka/xlm-roberta-base-english-upos https://github.com/UniversalDependencies/UD_English-EWT batch=8
 exit 0
