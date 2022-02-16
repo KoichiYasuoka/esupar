5ca229eb6b25ff770ff777e4faa8ecba00d177d3
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-02-16 12:19:14 +0900

    roberta-large-english-upos

--- train/en.sh
+++ train/en.sh
@@ -1,5 +1,11 @@
 #! /bin/sh
 pip3 install -U esupar pytokenizations
-python3 -m esupar.train roberta-base KoichiYasuoka/roberta-base-english-upos https://github.com/UniversalDependencies/UD_English-EWT
-python3 -m esupar.train xlm-roberta-base KoichiYasuoka/xlm-roberta-base-english-upos https://github.com/UniversalDependencies/UD_English-EWT batch=8
+for U in UD_English-EWT UD_English-GUM UD_English-ParTUT UD_English-Lines UD_English-Atis
+do test -d $U || git clone --depth=1 https://github.com/UniversalDependencies/$U
+done
+for F in train dev test
+do cat UD_English-*/*-$F.conllu > $F.conllu
+done
+python3 -m esupar.train roberta-base KoichiYasuoka/roberta-base-english-upos . batch=16
+python3 -m esupar.train roberta-large KoichiYasuoka/roberta-large-english-upos . batch=4
 exit 0
