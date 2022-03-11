2b02b1b1552b16614115525f3837c9a678086205
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-03-11 21:38:15 +0900

    bert-large-german-upos

--- train/de.sh
+++ train/de.sh
@@ -4,5 +4,6 @@ test -d UD_German-HDT || git clone --depth=1 https://github.com/UniversalDepende
 for F in train dev test
 do cat UD_German-HDT/*-$F*.conllu > $F.conllu
 done
-python3 -m esupar.train bert-base-german-cased KoichiYasuoka/bert-base-german-upos .
+python3 -m esupar.train deepset/gbert-base KoichiYasuoka/bert-base-german-upos .
+python3 -m esupar.train deepset/gbert-large KoichiYasuoka/bert-large-german-upos . batch=8
 exit 0
