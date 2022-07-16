e61054dd05aeeb40c6545bcef175950da1163797
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-07-16 17:35:59 +0900

    bug fix

--- train/th.sh
+++ train/th.sh
@@ -4,7 +4,7 @@ if [ -d spaCy-Thai ]
 then ( cd spaCy-Thai && git pull )
 else git clone --depth=1 https://github.com/KoichiYasuoka/spaCy-Thai
 fi
-s='{print>"train.upos";if(NF>0)u=u$0"\n";else{f=FILENAME;if(u~/\t0\troot\t/)print u>(f~/-dev/?"dev":f~/-test/?"test":f~/-train/?"train":NR%10<1?"dev":NR%10<2?"test":"train")".conllu";u=""}}'
+s='{if($6~/^NER=/)$6="_";print>"train.upos";if(NF>0)u=u$0"\n";else{f=FILENAME;if(u~/\t0\troot\t/)print u>(f~/-dev/?"dev":f~/-test/?"test":f~/-train/?"train":NR%10<1?"dev":NR%10<2?"test":"train")".conllu";u=""}}'
 nawk -F'\t' "$s" spaCy-Thai/UD_Thai-Corpora/*-ud-*.conllu
 
 python3 -m esupar.train KoichiYasuoka/roberta-base-thai-char KoichiYasuoka/roberta-base-thai-char-upos 32 /tmp train.upos
