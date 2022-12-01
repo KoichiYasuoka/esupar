66e9de194ff5ede28bb05988d0b53d5c7044ee0f
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-12-01 14:00:01 +0900

    한자 support

--- train/ko.sh
+++ train/ko.sh
@@ -5,6 +5,6 @@ test -d UD_Korean-GSD || git clone --depth=1 https://github.com/UniversalDepende
 for F in train dev test
 do cat UD_Korean-*/*-$F.conllu > $F.conllu
 done
-python3 -m esupar.train klue/roberta-base KoichiYasuoka/roberta-base-korean-upos .
-python3 -m esupar.train klue/roberta-large KoichiYasuoka/roberta-large-korean-upos .
+python3 -m esupar.train KoichiYasuoka/roberta-base-korean-hanja KoichiYasuoka/roberta-base-korean-upos .
+python3 -m esupar.train KoichiYasuoka/roberta-large-korean-hanja KoichiYasuoka/roberta-large-korean-upos .
 exit 0
--- train/ko_morph.sh
+++ train/ko_morph.sh
@@ -4,6 +4,6 @@ test -d morphUD-korean || git clone --depth=1 https://github.com/jungyeul/morphU
 for F in train dev test
 do cp morphUD-korean/data/ko_gsd-ud-$F-morph.conllu $F.conllu
 done
-python3 -m esupar.train klue/roberta-base KoichiYasuoka/roberta-base-korean-morph-upos .
-python3 -m esupar.train klue/roberta-large KoichiYasuoka/roberta-large-korean-morph-upos .
+python3 -m esupar.train KoichiYasuoka/roberta-base-korean-hanja KoichiYasuoka/roberta-base-korean-morph-upos .
+python3 -m esupar.train KoichiYasuoka/roberta-large-korean-hanja KoichiYasuoka/roberta-large-korean-morph-upos .
 exit 0
