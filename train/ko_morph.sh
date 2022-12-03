68ad38595b4f5c7008c9a625f0d9d8aab2375cb9
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-12-03 14:57:07 +0900

    aux links changed

--- train/ko_morph.sh
+++ train/ko_morph.sh
@@ -2,7 +2,25 @@
 pip3 install -U esupar pytokenizations
 test -d morphUD-korean || git clone --depth=1 https://github.com/jungyeul/morphUD-korean
 for F in train dev test
-do cp morphUD-korean/data/ko_gsd-ud-$F-morph.conllu $F.conllu
+do nawk '
+BEGIN{
+  IFS=OFS="\t";
+}
+{
+  if(NF==10){
+    if($8=="aux"){
+      if($4=="PART"){
+        if($5=="XSN")
+          $8="fixed";
+        else if($5!="XSV")
+          $8="mark";
+      }
+      else if($5=="VCP")
+        $8="cop";
+    }
+  }
+  print;
+}' morphUD-korean/data/ko_gsd-ud-$F-morph.conllu > $F.conllu
 done
 python3 -m esupar.train KoichiYasuoka/roberta-base-korean-hanja KoichiYasuoka/roberta-base-korean-morph-upos .
 python3 -m esupar.train KoichiYasuoka/roberta-large-korean-hanja KoichiYasuoka/roberta-large-korean-morph-upos .
