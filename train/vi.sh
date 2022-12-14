f8a4d56bbf0664b9da9ac62ebaa20b6de48d54c9
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-12-14 20:34:58 +0900

    roberta-base-vietnamese-upos

--- train/vi.sh
+++ train/vi.sh
@@ -6,7 +6,7 @@ from transformers import BertTokenizer,AutoModelForMaskedLM
 mdl=AutoModelForMaskedLM.from_pretrained("FPTAI/vibert-base-cased")
 tkz=BertTokenizer.from_pretrained("FPTAI/vibert-base-cased",do_lower_case=False,strip_accents=False,model_max_length=mdl.config.max_position_embeddings)
 tkz.save_pretrained("tmpdir")
-mdl.save_pretrained("tmpdir")
-'
+mdl.save_pretrained("tmpdir")'
 python3 -m esupar.train tmpdir KoichiYasuoka/bert-base-vietnamese-upos UD_Vietnamese-VTB
+python3 -m esupar.train KoichiYasuoka/roberta-base-vietnamese KoichiYasuoka/roberta-base-vietnamese-upos UD_Vietnamese-VTB
 exit 0
