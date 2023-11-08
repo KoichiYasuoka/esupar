e8869ff34e77bb18875b7de5167d714cde596efe
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2023-11-08 19:01:38 +0900

    spacy-alignments

--- train/ko.sh
+++ train/ko.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-pip3 install -U esupar pytokenizations
+pip3 install -U esupar spacy-alignments
 test -d UD_Korean-Kaist || git clone --depth=1 https://github.com/UniversalDependencies/UD_Korean-Kaist
 test -d UD_Korean-GSD || git clone --depth=1 https://github.com/UniversalDependencies/UD_Korean-GSD
 for F in train dev test
-do cat UD_Korean-*/*-$F.conllu > $F.conllu
+do sed 's/\(	않.*	\)VERB\(	.*	\)flat	/\1AUX\2aux	/' UD_Korean-*/*-$F.conllu > $F.conllu
 done
 python3 -m esupar.train KoichiYasuoka/roberta-base-korean-hanja KoichiYasuoka/roberta-base-korean-upos .
 python3 -m esupar.train KoichiYasuoka/roberta-large-korean-hanja KoichiYasuoka/roberta-large-korean-upos .
