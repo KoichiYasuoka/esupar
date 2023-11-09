2537812903825db0f843d35d1c5492f592f5f7bf
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2023-11-09 09:04:39 +0900

    model improved

--- train/ko.sh
+++ train/ko.sh
@@ -3,7 +3,21 @@ pip3 install -U esupar spacy-alignments
 test -d UD_Korean-Kaist || git clone --depth=1 https://github.com/UniversalDependencies/UD_Korean-Kaist
 test -d UD_Korean-GSD || git clone --depth=1 https://github.com/UniversalDependencies/UD_Korean-GSD
 for F in train dev test
-do sed 's/\(	않.*	\)VERB\(	.*	\)flat	/\1AUX\2aux	/' UD_Korean-*/*-$F.conllu > $F.conllu
+do nawk '
+BEGIN{
+  FS=OFS="\t";
+}
+{
+  if(NF==10){
+    if($4=="ADP"&&$8~/^(nsubj|obj)$/)
+      $8="case";
+    else if($2~/^않/&&$4=="VERB"&&$8=="flat"){
+      $4="AUX";
+      $8="aux";
+    }
+  }
+  print;
+}' UD_Korean-*/*-$F.conllu > $F.conllu
 done
 python3 -m esupar.train KoichiYasuoka/roberta-base-korean-hanja KoichiYasuoka/roberta-base-korean-upos .
 python3 -m esupar.train KoichiYasuoka/roberta-large-korean-hanja KoichiYasuoka/roberta-large-korean-upos .
