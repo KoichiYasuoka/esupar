14b192518057956757e999542da1fcc868cd90d8
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2024-08-08 13:21:54 +0900

    roberta-small-belarusian-upos

---
+++ train/be.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+test -d UD_Belarusian-HSE || git clone --depth=1 https://github.com/UniversalDependencies/UD_Belarusian-HSE
+for F in train dev test
+do cp UD_Belarusian-HSE/*-$F.conllu $F.conllu
+done
+cat *.conllu > train.upos
+python3 -m esupar.train KoichiYasuoka/roberta-small-belarusian KoichiYasuoka/roberta-small-belarusian-upos -32 /tmp train.upos
+python3 -m esupar.train KoichiYasuoka/roberta-small-belarusian-upos KoichiYasuoka/roberta-small-belarusian-upos 32 /// train.conllu dev.conllu test.conllu
