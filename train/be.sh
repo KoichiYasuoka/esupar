e5fea083fe972f70b33c1f9bcab455c6e3115504
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2024-08-13 09:56:24 +0900

    deberta-base-belarusian-upos

--- train/be.sh
+++ train/be.sh
@@ -6,3 +6,5 @@ done
 cat *.conllu > train.upos
 python3 -m esupar.train KoichiYasuoka/roberta-small-belarusian KoichiYasuoka/roberta-small-belarusian-upos -32 /tmp train.upos
 python3 -m esupar.train KoichiYasuoka/roberta-small-belarusian-upos KoichiYasuoka/roberta-small-belarusian-upos 32 /// train.conllu dev.conllu test.conllu
+python3 -m esupar.train KoichiYasuoka/deberta-base-belarusian KoichiYasuoka/deberta-base-belarusian-upos -32 /tmp train.upos
+python3 -m esupar.train KoichiYasuoka/deberta-base-belarusian-upos KoichiYasuoka/deberta-base-belarusian-upos 32 /// train.conllu dev.conllu test.conllu
